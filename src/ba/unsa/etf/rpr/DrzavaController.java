c3931f623564afbc9bf28ef27bf973679fbd0bd4
akantic1 <akantic1@etf.unsa.ba>
2020-02-16 22:01:32 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/DrzavaController.java
@@ -0,0 +1,98 @@
+package ba.unsa.etf.rpr;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class DrzavaController {
+    public TextField fieldNaziv;
+    public ChoiceBox<Grad> choiceGrad;
+    public ChoiceBox<Grad> choiceGradNajveci;
+    public RadioButton radioIsti, radioDrugi;
+
+    private Drzava drzava;
+    private ObservableList<Grad> listGradovi;
+
+    public DrzavaController(Drzava drzava, ArrayList<Grad> gradovi) {
+        this.drzava = drzava;
+        listGradovi = FXCollections.observableArrayList(gradovi);
+    }
+
+    @FXML
+    public void initialize() {
+        choiceGrad.setItems(listGradovi);
+        choiceGradNajveci.setItems(listGradovi);
+        if (drzava != null) {
+            fieldNaziv.setText(drzava.getNaziv());
+            Grad glavni = drzava.getGlavniGrad();
+            Grad najveci = drzava.getNajveciGrad();
+            for (Grad grad : listGradovi) {
+                if (grad.getId() == glavni.getId())
+                    choiceGrad.getSelectionModel().select(grad);
+                if (grad.getId() == najveci.getId())
+                    choiceGradNajveci.getSelectionModel().select(grad);
+            }
+            if (glavni.getId() == najveci.getId())
+                radioIsti.setSelected(true);
+            else
+                radioDrugi.setSelected(true);
+        } else {
+            choiceGrad.getSelectionModel().selectFirst();
+            choiceGradNajveci.getSelectionModel().selectFirst();
+            choiceGradNajveci.setDisable(true);
+            radioIsti.setSelected(true);
+        }
+    }
+
+    public Drzava getDrzava() {
+        return drzava;
+    }
+
+    public void clickOk(ActionEvent actionEvent) {
+        boolean sveOk = true;
+
+        if (fieldNaziv.getText().trim().isEmpty()) {
+            fieldNaziv.getStyleClass().removeAll("poljeIspravno");
+            fieldNaziv.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        } else {
+            fieldNaziv.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldNaziv.getStyleClass().add("poljeIspravno");
+        }
+
+        if (!sveOk) return;
+
+        if (drzava == null) drzava = new Drzava();
+        drzava.setNaziv(fieldNaziv.getText());
+        drzava.setGlavniGrad(choiceGrad.getSelectionModel().getSelectedItem());
+        if (radioIsti.isSelected())
+            drzava.setNajveciGrad(choiceGrad.getSelectionModel().getSelectedItem());
+        else
+            drzava.setNajveciGrad(choiceGradNajveci.getSelectionModel().getSelectedItem());
+        closeWindow();
+    }
+
+    public void clickCancel(ActionEvent actionEvent) {
+        drzava = null;
+        closeWindow();
+    }
+
+    public void clickRadio(ActionEvent actionEvent) {
+        if (radioIsti.isSelected())
+            choiceGradNajveci.setDisable(true);
+        else
+            choiceGradNajveci.setDisable(false);
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) fieldNaziv.getScene().getWindow();
+        stage.close();
+    }
+}
